2f70cb1599aa4f0eb3b99ba9ad9e0629b7e1b0ca
Søren Berg Glasius <soeren@glasius.dk>
2016-09-15 15:06:31 +0200

    Added travis-build.sh

---
+++ travis-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+./gradlew -q clean check install --stacktrace
+
+# If a tag is set, upload to BinTray
+if [[ -n $TRAVIS_TAG && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+	./gradlew bintrayUpload --stacktrace
+fi
