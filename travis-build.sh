da09dd7c24e89e371496c13ff1129d13f170bdab
Søren Berg Glasius <soeren@glasius.dk>
2016-09-15 16:18:31 +0200

    Removed quiet flag from first gradle execution

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-./gradlew -q clean check install --stacktrace --console plain
+./gradlew clean check install --stacktrace 
 
 # If a tag is set, upload to BinTray
 if [[ -n $TRAVIS_TAG && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
