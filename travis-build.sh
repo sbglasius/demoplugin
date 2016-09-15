6589abdca95d81cf96c1d87ddfb9446283d595ff
Søren Berg Glasius <soeren@glasius.dk>
2016-09-15 16:07:11 +0200

    Added --console plain to gradle output

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-./gradlew -q clean check install --stacktrace
+./gradlew -q clean check install --stacktrace --console plain
 
 # If a tag is set, upload to BinTray
 if [[ -n $TRAVIS_TAG && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
