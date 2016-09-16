33cf5e8e9f10571aff549535cc26988527415670
Søren Berg Glasius <soeren@glasius.dk>
2016-09-16 14:33:37 +0200

    Echo Travis vars

--- travis-build.sh
+++ travis-build.sh
@@ -3,6 +3,9 @@
 set -e 
 
 ./gradlew clean check install --stacktrace 
+echo "TRAVIS_TAG          : $TRAVIS_TAG"
+echo "TRAVIS_BRANCH       : $TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
 
 # If a tag is set, upload to BinTray
 if [[ -n $TRAVIS_TAG && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
